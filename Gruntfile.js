0716fd4e770b10869024242de67ea581bca750ca
Matthew Harrigan <harrigan@stanford.edu>
2016-04-20 18:13:31 -0700

    Customize JS
     - Remove extraneous modules

--- Gruntfile.js
+++ Gruntfile.js
@@ -195,16 +195,7 @@ module.exports = function (grunt) {
       bootstrap: {
         src: [
           'js/src/util.js',
-          'js/src/alert.js',
-          'js/src/button.js',
-          'js/src/carousel.js',
           'js/src/collapse.js',
-          'js/src/dropdown.js',
-          'js/src/modal.js',
-          'js/src/scrollspy.js',
-          'js/src/tab.js',
-          'js/src/tooltip.js',
-          'js/src/popover.js'
         ],
         dest: 'dist/js/<%= pkg.name %>.js'
       }
